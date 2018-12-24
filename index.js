e0eb32328b4d262d85dfa7e7c124b6cb9e34fc63
VizardCrawler (Vishal Kumar Mitra) <vishal.kumarmitra786@gmail.com>
2018-12-24 08:39:21 -0500

    Add files via upload
    added null check

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ $(function () {
     var url = new URL(url_string);
     var name = url.searchParams.get("name");
     console.log(name);
-    $('#name').append('~from ' + name);
+    $('#name').append('~from ' + name ? name : 'santa');
     $('.tlt').textillate({ in: { effect: 'rollIn' } });
 
     $('.share').click(function (evt) {
-        alert("hello");
         var base_url = window.location.origin;
         var shareMsg = base_url + '?name=' + $('.xmas-input').val();
         console.log(shareMsg);
