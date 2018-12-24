43e80d3cade8141d3da076173dba3ea44bf01c4a
VizardCrawler (Vishal Kumar Mitra) <vishal.kumarmitra786@gmail.com>
2018-12-24 10:10:44 -0500

    Add files via upload
    Added share changes

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ $(function () {
     var url_string = window.location.href; //window.location.href
     var url = new URL(url_string);
     var name = url.searchParams.get("name");
-    console.log(name);
     $('#name').append('~from ' + name ? name : 'santa');
     $('.tlt').textillate({ in: { effect: 'rollIn' } });
 
     $('.share').click(function (evt) {
         var base_url = window.location.origin;
-        var shareMsg = base_url + '/xmas?name=' + $('.xmas-input').val() ? $('.xmas-input').val() : 'santa';
+        var shareMsg = base_url + "/xmas?name=" + ($('.xmas-input').val() ? $('.xmas-input').val() : 'santa');
         console.log(shareMsg);
         var anchor = $('.xmas-a');
         anchor.attr('href', "whatsapp://send?text=" + shareMsg);
